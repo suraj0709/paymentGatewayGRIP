9c154696cdfd98a20f054d295472027124dff884
Suraj Kumar <43446354+suraj0709@users.noreply.github.com>
2021-04-17 10:18:27 +0530

    final commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Sun Rise Foundation</title>
+</head>
+<body>
+<div class="overlay">
+    <div class="grid-container">
+    <div class="grid-item"></div>
+    <div class="grid-item">
+        <img src="https://d1vdjc70h9nzd9.cloudfront.net/media/campaign/115000/115153/image/5d35baafa2676.jpeg" class='bg'>
+        <img src="https://dkprodimages.gumlet.io/campaign/VCT_Educational_material_Supplies_CoverPic.jpg?format=webp&w=400&dpr=2.6" class='bg'>
+        <img src="https://i2.wp.com/arunachal24.in/wp-content/uploads/2020/09/ABK-blood-donation.jpg?fit=800%2C400&ssl=1" class='bg'>
+        <img src="https://www.careindia.org/wp-content/themes/careindia/images/donate-sec-women.jpg" class='bg'>
+        <img src="https://www.smilefoundationindia.org/images/donation/kalahandi-health.jpg" class='bg'>
+        <img src="https://cdn.givind.org/static/images/categorypage/v3/mobile/mobile-cancer.jpg" class='bg'>
+        <img src="https://s01.sgp1.cdn.digitaloceanspaces.com/article/125053-nvcrhtfkgy-1565011785.jpg" class='bg'>
+        <img src="https://iq-images-processed.s3.amazonaws.com/uploads/made/93df1acbda975f33eda8590de125ab262c83aa37.jpg" class='bg'>
+    </div> 
+    </div>
+    <div class="m-main">
+        <div class="main">
+            <h2>SAVE NEEDY PEOPLE FROM A LIFE OF MISERY!</h2>
+            <h4>HELPED BY US</h4>
+            <p>We got established in the year 2012 with a firm and sole motive to help the needy and poor children as much as we can. We work towards the implementation of welfare initiatives for the underprivileged community. We mainly address the causes of education & health of underprivileged children as we think that Education alone is the power that can redeem the poor from their poverty. The entire management process of SunRise Foundation is evaluated by a 4 tier audit system – programme and project audit, internal and process audit, statutory audit and external audit, which ensure the correct utilization of funds. We take up individual cases based on their immediate needs. These individuals are then given educational support, support for their health etc.?</p>
+
+            <div class="razorpay-embed-btn" data-url="https://pages.razorpay.com/pl_GzeE9WKgTv3bqG/view" data-text="Donate Now" data-color="#870943" data-size="large">
+            <script>
+                (function(){
+                var d=document; var x=!d.getElementById('razorpay-embed-btn-js')
+                if(x){ var s=d.createElement('script'); s.defer=!0;s.id='razorpay-embed-btn-js';
+                s.src='https://cdn.razorpay.com/static/embed_btn/bundle.js';d.body.appendChild(s);} else{var rzp=window['__rzp__'];
+                rzp && rzp.init && rzp.init()}})();
+            </script>
+            </div>
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
+
+
+    
